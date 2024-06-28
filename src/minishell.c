37ed3289aef0be34fa50a613d1b07d6da32a8c98
younesscoder01 <younesscoder@gmail.com>
2024-06-28 19:05:58 +0100

    some edit in the parsing part

--- src/minishell.c
+++ src/minishell.c
@@ -6,17 +6,24 @@
 /*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:01:06 by ysahraou          #+#    #+#             */
-/*   Updated: 2024/06/28 18:10:53 by ysahraou         ###   ########.fr       */
+/*   Updated: 2024/06/28 18:55:05 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main()
+int main(int argc, char **argv, char **env)
 {
     char    *line;
     char    **args;
+    char    **paths;
 
+    (void)argc;
+    (void)argv;
+    
+    paths = get_paths(env);
+    while (*paths)
+        printf("%s\n", *paths++);
     while (1)
     {
         line = readline("Minishell$");
