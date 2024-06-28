efa1ad1e86a82cf474baa2ed46c810c9a047e51c
younesscoder01 <younesscoder@gmail.com>
2024-06-28 18:13:15 +0100

    Display a prompt

--- src/minishell.c
+++ src/minishell.c
@@ -1,12 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 18:01:06 by ysahraou          #+#    #+#             */
+/*   Updated: 2024/06/28 18:10:53 by ysahraou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
 int main()
 {
-    char *cmd;
-    char **tab;
+    char    *line;
+    char    **args;
 
-    cmd = readline("minishell_$ ");
-    tab = ft_split(cmd, ' ');
-
-    return(0);
+    while (1)
+    {
+        line = readline("Minishell$");
+        args = ft_split(line, ' ');
+        // print the args
+        for (int i = 0; args[i]; i++)
+            printf("%s\n", args[i]);
+        add_history(line);
+    }
+    return (0);
 }
