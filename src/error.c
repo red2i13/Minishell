17a02dfdf5a2a6c391ad6ee7348700e16dfc5792
younesscoder01 <younesscoder@gmail.com>
2024-06-29 11:41:51 +0100

    add the error_func

--- src/error.c
+++ src/error.c
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/28 18:33:16 by ysahraou          #+#    #+#             */
-/*   Updated: 2024/06/29 11:41:03 by ysahraou         ###   ########.fr       */
+/*   Created: 2024/06/29 10:19:25 by ysahraou          #+#    #+#             */
+/*   Updated: 2024/06/29 10:29:08 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../includes/minishell.h"
 
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include "../libft/libft.h"
-# include <unistd.h>
-
-/*parsing.c*/
-int check_cmd(char *cmd);
-char **get_paths(char **env);
-
-/*error.c*/
-void    error_func(int errnum, int exit_num);
-
-#endif
+void error_func(int errnum, int exit_num)
+{
+    char str_err;
+    
+    str_err = strerror(errnum);
+    printf("%s", str_err);
+    exit(exit_num);
+}
