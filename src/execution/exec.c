4604abd25cf353f3e7a3da57b1affc6a29e6c2c6
red2i13 <redaben2001@gmail.com>
2024-09-01 23:18:02 +0100

    fix exit codes and bash behavior

--- src/execution/exec.c
+++ src/execution/exec.c
@@ -6,12 +6,19 @@
 /*   By: rbenmakh <rbenmakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 12:38:20 by ysahraou          #+#    #+#             */
-/*   Updated: 2024/08/30 17:09:22 by rbenmakh         ###   ########.fr       */
+/*   Updated: 2024/09/01 23:14:45 by rbenmakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void printf_error(char *str, char *cmd)
+{
+    write(2, cmd, ft_strlen(cmd));
+    write(2, ": ", 2);
+    write(2, str, ft_strlen(str));
+    write(2, "\n", 1);
+}
 char *check_cmd(char *cmd, char **paths)
 {
     int num; 
@@ -28,6 +35,12 @@ char *check_cmd(char *cmd, char **paths)
     }
     else if(cmd[0] == '\0')
 		return(NULL);
+    else if(cmd[0] == '.' && access(cmd, F_OK) != 0)
+    {
+        printf_error("No such file or directory", cmd);
+        g_status = 127;
+        return(NULL);
+    }
     while(paths[i])
     {
 		tmp = ft_strjoin( "/", cmd);
@@ -46,10 +59,9 @@ char *check_cmd(char *cmd, char **paths)
     if(num == -1 && check_builtin(cmd))
 	{
         //this function should store the exit code if the command is not found
-		printf("$: command not found: %s\n", cmd);
+		printf_error("command not found ", cmd);
 		cmd = NULL ;
         g_status = 127;
-        //error_func(errno, 127);
 	}
 	return(cmd);
 }
@@ -74,30 +86,30 @@ int check_redir(t_token *head, int f)
 }
 int builtin(t_token *head, t_list **envl, t_list **exp_list)
 {
-    if(ft_strnstr(head->args[0], "cd", 3))
+    if(!ft_strncmp(head->args[0], "cd", 3))
         return(cd(head->args, envl, exp_list), 0);
-    else if(ft_strnstr(head->args[0], "echo", 5))
+    else if(!ft_strncmp(head->args[0], "echo", 5))
         return(echo(head->args), 0);
-    else if(ft_strnstr(head->args[0], "export", 7))
+    else if(!ft_strncmp(head->args[0], "export", 7))
     {
         if(!head->args[1])
             export(exp_list, envl, NULL, NULL);
         init_export(head, envl, exp_list);
         return(0);
     }
-    else if(ft_strnstr(head->args[0], "unset", 6))
+    else if(!ft_strncmp(head->args[0], "unset", 6))
     {
         unset(envl, head->args[1], 0);
         unset(exp_list, head->args[1], 1); 
         return(0);
     }
-    else if(ft_strnstr(head->args[0], "env", 4) && !head->args[1])
+    else if(!ft_strncmp(head->args[0], "env", 4) && !head->args[1])
         return(print_env(*envl), 0);
-    else if(ft_strnstr(head->args[0], "env", 4) && head->args[1])
+    else if(!ft_strncmp(head->args[0], "env", 4) && head->args[1])
         return(write(2, "env: too many arguments\n", 25),0);
-    else if(ft_strnstr(head->args[0], "pwd", 4) && !head->args[1])
+    else if(!ft_strncmp(head->args[0], "pwd", 4) && !head->args[1])
         return(pwd(1, *envl), 0);
-    else if(ft_strnstr(head->args[0], "pwd", 4) && head->args[1])
+    else if(!ft_strncmp(head->args[0], "pwd", 4) && head->args[1])
         return(write(2, "pwd: too many arguments\n", 25),0);        
     return(1);
 }
@@ -111,7 +123,7 @@ void run_cmd(t_token *head, t_list **envl, t_list **exp_list ,char **paths)
     
     exit_st = 0;
     env = convert_to_array(*envl);
-    if (ft_strnstr(head->args[0], "exit", ft_strlen("exit")))
+    if (!ft_strncmp(head->args[0], "exit", 5))
     {
         free_arr(paths);
         free_arr(env);
--- src/execution/pipes.c
+++ src/execution/pipes.c
@@ -6,7 +6,7 @@
 /*   By: rbenmakh <rbenmakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 20:50:47 by rbenmakh          #+#    #+#             */
-/*   Updated: 2024/08/29 15:20:54 by rbenmakh         ###   ########.fr       */
+/*   Updated: 2024/09/01 22:55:32 by rbenmakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ void run(t_token *head, t_list **envl, t_list **exp_list ,char **paths)
     char *cmd;
     char **env ;
     
-    cmd = check_cmd(head->args[0], paths);
     int r;
     if((r = check_redir(head, 0)))
     {
@@ -65,39 +64,95 @@ void run(t_token *head, t_list **envl, t_list **exp_list ,char **paths)
             tmp = tmp->next;
         }
     }
-    if(!cmd)
-        exit(127);
     env = convert_to_array(*envl);
     if (ft_strnstr(head->args[0], "exit", ft_strlen("exit")))
-            ft_exit(head);
-    else if(ft_strnstr(head->args[0], "cd", 3))
-        cd(head->args, envl, exp_list);
-    else if(ft_strnstr(head->args[0], "echo", 5))
-        echo(head->args);
-    else if(ft_strnstr(head->args[0], "pwd", 4))
-        pwd(1, *envl);
-    else if(ft_strnstr(head->args[0], "export", 7))
     {
-        if(!head->args[1])
-            export(exp_list, envl, NULL, NULL);
-        init_export(head, envl, exp_list);
+        free_arr(paths);
+        free_arr(env);
+        env = NULL;
+        //free envl and exp list
+        ft_exit(head);
     }
-    else if(ft_strnstr(head->args[0], "unset", 6))
+    else if(!builtin(head, envl, exp_list))
     {
-        unset(envl, head->args[1], 0);
-        unset(exp_list, head->args[1], 1); 
+        free_arr(paths);
+        free(env);
+        env = NULL;
+        return ;
     }
-    else if(ft_strnstr(head->args[0], "env", 4))
-        print_env(*envl);
+    cmd = check_cmd(head->args[0], paths);
+    if(!cmd)
+        exit(127);
+
     else if(execve(cmd, head->args, env) == -1)
     {
         perror("minishell: ");
         exit(EXIT_FAILURE);
     }
+    if(cmd != head->args[0])
+        free(cmd);
     free_arr(paths);
     free_arr(env);
+    exit(0);
 }
 
+
+// int exec_pipes(t_token *head, t_list **envl, t_list **exp_list ,char **paths)
+// {
+//     int i;
+//     int pid;
+//     int exit_st;    
+//     int p = calc_pipes(head);
+//     int **fdt = init_pipes(p);
+   
+//     i = p;
+//     while(head->next)
+//     {
+//         head = head->next;
+//     }
+//     while(i >= 0)
+//     {
+//         if(!(pid = fork()))
+//         {
+//             if(i != p)
+//             {
+//                 dup2(fdt[i][1], STDOUT_FILENO);
+//             }
+//             if(i > 0)
+//             {
+//                 dup2(fdt[i - 1][0], STDIN_FILENO);
+//             }
+//             //function that kill all the unused file descriptor
+//             for(int k = 0; k < p; k++)
+//             {
+//                 close(fdt[k][0]);
+//                 close(fdt[k][1]);
+//             }
+//             run(head, envl, exp_list, paths);
+//             exit(0);
+//         }
+//         if(head->prev)
+//             head = head->prev->prev;
+//         i--;
+//     }
+//     for (i = 0; i < p; i++) 
+//     {
+//         close(fdt[i][0]);
+//         close(fdt[i][1]);
+//     }
+//     while(wait(&exit_st) > 0)
+//     {   
+//         printf("status %i\n", WEXITSTATUS(exit_st));
+//     }
+//     for (i = 0; i < p; i++) 
+//     {
+//         free(fdt[i]);
+//     }
+//     free(fdt);
+//     g_status = exit_st / 256;
+//     free_arr(paths);
+//     return(0);
+// }
 
 int exec_pipes(t_token *head, t_list **envl, t_list **exp_list ,char **paths)
 {
@@ -106,13 +161,11 @@ int exec_pipes(t_token *head, t_list **envl, t_list **exp_list ,char **paths)
     int exit_st;    
     int p = calc_pipes(head);
     int **fdt = init_pipes(p);
-   
-    i = p;
-    while(head->next)
-    {
-        head = head->next;
-    }
-    while(i >= 0)
+    //to test
+    int last_pid;
+    i = 0;
+
+    while(i <= p)
     {
         if(!(pid = fork()))
         {
@@ -131,26 +184,31 @@ int exec_pipes(t_token *head, t_list **envl, t_list **exp_list ,char **paths)
                 close(fdt[k][1]);
             }
             run(head, envl, exp_list, paths);
-            exit(0);
         }
-        if(head->prev)
-            head = head->prev->prev;
-        i--;
+        else if(i == p)
+        {
+            printf("the command is %s\n", head->args[0]);
+            last_pid = pid;
+        }
+        if(head->next)
+            head = head->next->next;
+        i++;
     }
     for (i = 0; i < p; i++) 
     {
         close(fdt[i][0]);
         close(fdt[i][1]);
     }
-    while(wait(&exit_st) > 0)
-    {   
+    waitpid(last_pid, &exit_st, 0);
+    while(wait(NULL) > 0)
+    {
     }
+    g_status = WEXITSTATUS(exit_st);
     for (i = 0; i < p; i++) 
     {
         free(fdt[i]);
     }
     free(fdt);
-    g_status = exit_st / 256;
-
+    free_arr(paths);
     return(0);
 }
