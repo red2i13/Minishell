6c846eea25f08a4aa6a8e9e17947519d22991335
younesscoder01 <younesscoder@gmail.com>
2024-09-08 16:26:20 +0100

    add the exit code in the prompt

--- src/parser/prompt.c
+++ src/parser/prompt.c
@@ -6,17 +6,49 @@
 /*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 18:21:09 by ysahraou          #+#    #+#             */
-/*   Updated: 2024/09/06 18:21:29 by ysahraou         ###   ########.fr       */
+/*   Updated: 2024/09/08 16:16:19 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+char *color_g(char *user, char *exit_s)
+{
+    char *temp;
+    char *ret;
+
+    ret = ft_strjoin(user, "@\033[0;32mminishell [");
+    temp = ret;
+    ret = ft_strjoin(ret, exit_s);
+    free(temp);
+    temp = ret;
+    ret = ft_strjoin(ret, "]→\033[0m ");
+    free(temp);
+    return (ret);
+}
+
+char *color_r(char *user, char *exit_s)
+{
+    char *temp;
+    char *ret;
+
+    ret = ft_strjoin(user, "@\033[0;32mminishell\033[0;31m [");
+    temp = ret;
+    ret = ft_strjoin(ret, exit_s);
+    free(temp);
+    temp = ret;
+    ret = ft_strjoin(ret, "]→\033[0m ");
+    free(temp);
+    return (ret);
+}
+
 char *get_user(t_list  *env, int err)
 {
     char *user;
     char *tmp;
+    char *exit_s;
 
+    exit_s = ft_itoa(g_status);
     user = get_var("USER", env);
     if (!user)
         user = ft_strdup("gest");
@@ -25,10 +57,11 @@ char *get_user(t_list  *env, int err)
     free(tmp);
     tmp = user;
     if (!err)
-        user = ft_strjoin(user, "@\033[0;32mminishell →\033[0m ");
+        user = color_g(user, exit_s);
     else
-        user = ft_strjoin(user, "@\033[0;32mminishell\033[0;31m →\033[0m ");
+        user = color_r(user, exit_s);
     free(tmp);
+    free(exit_s);
     return (user);
 }
 
