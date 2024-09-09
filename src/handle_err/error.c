5e20902167f24428ca9a200e0d0ebce183e10ae0
younesscoder01 <younesscoder@gmail.com>
2024-09-09 17:49:12 +0100

    norm again

--- src/handle_err/error.c
+++ src/handle_err/error.c
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbenmakh <rbenmakh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 10:19:25 by ysahraou          #+#    #+#             */
-/*   Updated: 2024/08/07 09:48:47 by rbenmakh         ###   ########.fr       */
+/*   Updated: 2024/09/09 17:48:03 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void error_func(int errnum, int exit_num)
+void	error_func(int errnum, int exit_num)
 {
-    char *str_err;
-    
-    str_err = strerror(errnum);
-    printf("%s\n", str_err);
-    (void)exit_num;
-    //fix: if i run cd in the main process the shell exit
-    //exit(exit_num);
+	char	*str_err;
+
+	str_err = strerror(errnum);
+	printf("%s\n", str_err);
+	(void)exit_num;
+	// fix: if i run cd in the main process the shell exit
+	// exit(exit_num);
 }
 
-void error_exit(char *str, int exit_num)
+void	error_exit(char *str, int exit_num)
 {
-    write(2, str, ft_strlen(str));
-    exit(exit_num);
+	write(2, str, ft_strlen(str));
+	exit(exit_num);
 }
