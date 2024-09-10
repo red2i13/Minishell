147547f0ebbe6642574072b754269ba6d96b4ead
younesscoder01 <younesscoder@gmail.com>
2024-09-10 11:16:02 +0100

    add the c option to the shell

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:01:06 by ysahraou          #+#    #+#             */
-/*   Updated: 2024/09/10 10:22:17 by ysahraou         ###   ########.fr       */
+/*   Updated: 2024/09/10 10:53:46 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,24 @@ int main(int argc, char **argv, char **env)
     char    *line;
     t_token *head;
     t_list  *envl;
+    int c_option_f;
     t_list  *exp_list;
 
     set_up_env_exp(&envl, &exp_list, env);
+    c_option_f = 0;
     while (argc && argv)
     {
         signal_setup(2);
         restor_history(envl);
-        line = prompt(envl);
+        if (c_option_f)
+            return (g_status);
+        if (argc == 3 && ff_strncmp("-c", argv[1], ft_strlen("-c")))
+        {
+            line = argv[2];
+            c_option_f  = 1;
+        }
+        else
+            line = prompt(envl);
         if (!line)
             return (ctl_exit(exp_list, envl));
         if (line[0] == '\0')
