84f9b65980873a3d7025f67eae30f8293ccf5734
younesscoder01 <younesscoder@gmail.com>
2024-09-14 10:36:06 +0100

    ^C in cat

--- src/execution/exec_utiles.c
+++ src/execution/exec_utiles.c
@@ -6,7 +6,7 @@
 /*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 14:46:01 by ysahraou          #+#    #+#             */
-/*   Updated: 2024/09/12 14:47:16 by ysahraou         ###   ########.fr       */
+/*   Updated: 2024/09/14 10:30:51 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,9 @@ void	exit_status(int status)
 	if (WIFEXITED(status))
 		g_status = WEXITSTATUS(status);
 	else if (WIFSIGNALED(status))
+	{
 		g_status = WTERMSIG(status) + 128;
+		if (g_status == 130)
+			write(1, "\n", 1);
+	}
 }
